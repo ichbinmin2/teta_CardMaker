e645849131f633c44f0d3817cebad9f883196e67
JiyeonMin <ichbinmin2@gmail.com>
2021-02-10 17:36:32 +0900

    Add : card & edit form component

--- src/components/card-edit/card-edit.jsx
+++ src/components/card-edit/card-edit.jsx
@@ -1,33 +1,74 @@
 import React from "react";
+import Button from "../button/button";
 import styles from "../card-edit/card-edit.module.css";
+import ImageInput from "../image_input/image_input";
 
 const CardEdit = ({ card }) => {
+  const {
+    id,
+    name,
+    company,
+    theme,
+    title,
+    email,
+    message,
+    fileName,
+    fileURL,
+  } = card;
+
   // const inputRef = React.createRef();
   // const onSubmit = (event) => {
   //   event.preventDefault();
-  //   const editedText = inputRef.current.value;
+  //   const editedText = inputRef.
   //   editedText && props.onAdd(editedText);
   //   inputRef.current.value = "";
   // };
-
+  const onSubmit = () => {};
   return (
-    <section>
-      <form className={styles.writeTable}>
-        <h1>{card.id}</h1>
-        {/* <input ref={inputRef} type="text" placeholder="이름">{}</input>
-        <input ref={inputRef} type="text" placeholder="직장"></input>
-        <select>
-          <option>Dark</option>
-          <option>White</option>
-          <option>Colorful</option>
-        </select>
-        <input ref={inputRef} type="text" placeholder="직업"></input>
-        <input ref={inputRef} type="email" placeholder="이메일 주소"></input>
-        <textarea>메모</textarea>
-        <button onClick={onSubmit}>이름버튼</button>
-        <button>리셋버튼</button> */}
-      </form>
-    </section>
+    <form className={styles.writeform}>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        placeholder="name"
+        className={styles.input}
+      ></input>
+      <input
+        type="text"
+        name="company"
+        value={company}
+        placeholder="직장"
+        className={styles.input}
+      ></input>
+      <select //
+        name="theme"
+        value={theme}
+        className={styles.select}
+      >
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">Colorful</option>
+      </select>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        placeholder="직업"
+        className={styles.input}
+      ></input>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        placeholder="이메일 주소"
+        className={styles.input}
+      ></input>
+      <textarea value={message} className={styles.message}></textarea>
+      <div className={styles.inputBtn}>
+        <ImageInput />
+      </div>
+      <Button name="Delete" onClick={onSubmit} />
+    </form>
   );
 };
 
---
+++ src/components/image_input/image_input.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../image_input/image_input.module.css";
+
+const ImageInput = (props) => (
+  <button className={styles.imageBtn}>image</button>
+);
+
+export default ImageInput;
