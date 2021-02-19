80de0cc3d30c63f4167e3e6be5fe500fec5c9c4f
JiyeonMin <ichbinmin2@gmail.com>
2021-02-20 00:17:54 +0900

    Modify : style detail

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,21 +1,33 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { useHistory } from "react-router-dom";
 import styles from "../pages/main.module.css";
 
 const Main = memo(() => {
   const history = useHistory();
+  const [start, setStart] = useState(false);
 
   const goToLoginPage = () => {
+    setStart(true);
     history.push("/board");
   };
 
   return (
     <section className={styles.background}>
-      <div>
-        <button onClick={goToLoginPage} className={styles.btn}>
+      {start !== true ? (
+        <button
+          onClick={goToLoginPage}
+          className={`${styles.btn} ${styles.after}`}
+        >
           Card Maker Start!
         </button>
-      </div>
+      ) : (
+        <button
+          onClick={goToLoginPage}
+          className={`${styles.btn} ${styles.before}`}
+        >
+          Card Maker Start!
+        </button>
+      )}
     </section>
   );
 });
